f3b98249a1bcae2df99eccfe7b0b316912188b69
2017tmxkxm <2017tmxkxm@gmail.com>
2023-09-04 14:50:16 +0900

    스프링 컨테이너로 통합

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -10,21 +10,29 @@ public class HellobootApplication {
 
 	public static void main(String[] args) {
 		// 스프링 컨테이너 생성, DispatcherServlet은 GenericWebApplicationContext를 사용
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+		// 서블릿 컨테이너 초기화 작업을 스프링 컨테이너 초기화 작업에 종속
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+				
+				// 서블릿 등록
+				ServletWebServerFactory serverFactory =  new TomcatServletWebServerFactory();
+				// getWebServer : servlet 컨테이너를 만드는 생성 함수
+				// ServletContextInitializer : selvlet 컨테이너에 servlet 연결 
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this)).addMapping("/*");
+				});
+				
+				// Tomcat servlet 컨테이너 동작
+				webServer.start();
+			}
+		};
+		
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
 		
-		// 서블릿 등록
-		ServletWebServerFactory serverFactory =  new TomcatServletWebServerFactory();
-		// getWebServer : servlet 컨테이너를 만드는 생성 함수
-		// ServletContextInitializer : selvlet 컨테이너에 servlet 연결 
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext)).addMapping("/*");
-		});
-		
-		// Tomcat servlet 컨테이너 동작
-		webServer.start();
 	}
 
 }
